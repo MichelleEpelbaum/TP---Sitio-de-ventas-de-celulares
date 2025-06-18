036aa7e3e5d7e5feb52495107e610f4ce1255f5d
Android Studio <devandroid@ort>
2025-06-18 10:20:55 -0300

    no corre

--- TP_celulares/src/App.jsx
+++ TP_celulares/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Nosotros from "./components/Nosotros";
+import Contacto from "./components/Contacto";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/quienes-somos/" element={<Nosotros />}></Route>
+        <Route path="/contacto" element={<Contacto />}></Route>
+        <Route path="*" element={<h1>404</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
